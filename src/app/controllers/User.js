f30254f4cc59cb9157e8e6dadce54e42d9ebcd28
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-16 22:23:48 -0300

    feat: Add store method in User Controller
    Create user in database with encrypted password.

---
+++ src/app/controllers/User.js
@@ -0,0 +1,22 @@
+import User from '../models/User';
+
+class UserController {
+  async store(request, response) {
+    const userExists = await User.findOne({
+      where: { email: request.body.email },
+    });
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email } = await User.create(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
