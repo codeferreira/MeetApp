413fb639ad44b6211a8275f2d5f0b024de716b4c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-18 23:58:24 -0300

    feat: Add update user route

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ import UserController from './app/controllers/User';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
