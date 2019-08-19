ada850fd21fb7959eea066b49ce1f02db82dd71a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-19 18:01:31 -0300

    feat: Add route to create session token

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/User';
+import SessionController from './app/controllers/Session';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
