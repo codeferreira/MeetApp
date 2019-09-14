f701af18a5a77a1375125cf3ba93f197bd323894
José JC. Carlos <jose_carlos@DEV02.INTRANET.LESTE>
2019-09-14 10:23:12 -0300

    feat: File uploader

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,14 +2,19 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/User';
 import SessionController from './app/controllers/Session';
+import FileController from './app/controllers/File';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.post('/files', FileController.store);
 
 export default routes;
