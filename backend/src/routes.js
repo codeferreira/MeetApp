78fb7d99aa7ff5922b062a3f78a714ebf971f1df
José JC. Carlos <jose_carlos@DEV02.INTRANET.LESTE>
2019-09-14 10:39:56 -0300

    chore: Multer config

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/User';
 import SessionController from './app/controllers/Session';
@@ -7,6 +9,7 @@ import FileController from './app/controllers/File';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -15,6 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', FileController.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
