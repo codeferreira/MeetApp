8c3926265eec2050179cf4bf5e25aa095744d0bb
José JC. Carlos <jose_carlos@DEV02.INTRANET.LESTE>
2019-09-17 16:33:32 -0300

    chore: Add meetup routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,11 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/User';
 import SessionController from './app/controllers/Session';
+
 import FileController from './app/controllers/File';
+import MeetupController from './app/controllers/Meetup';
+import OrganizingController from './app/controllers/Organizing';
+import SubscriptionController from './app/controllers/Subscription';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +23,15 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/meetups', MeetupController.index);
+routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/organizing', OrganizingController.index);
+routes.get('/subscriptions', SubscriptionController.index);
+
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
 
 export default routes;
