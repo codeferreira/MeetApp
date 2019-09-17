d7f389d42966eed82cd30a61a30f254994d13378
José JC. Carlos <jose_carlos@DEV02.INTRANET.LESTE>
2019-09-17 16:35:43 -0300

    Associate model

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,20 +1,33 @@
 import Sequelize from 'sequelize';
 
-import User from '../app/models/User';
-import File from '../app/models/File';
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
+
+const models = [User, File, Meetup];
 
 class Database {
   constructor() {
+    this.connection = new Sequelize(databaseConfig);
+
     this.init();
+    this.associate();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
   }
 }
+
 
 export default new Database();
